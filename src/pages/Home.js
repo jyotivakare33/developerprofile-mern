80b86c38d3993154b6c3d1cfe301b27548ab19cc
JYOTI VAKARE <30766323+jyotivakare33@users.noreply.github.com>
2021-04-12 17:38:17 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function Home() {
     return (
         <div>
             <MetaTags>
-            <title>Page 1</title>
+            <title>Developer Profile</title>
             <meta name="description" content="Some description." />
             <meta property="og:title" content="MyApp" />
             <meta property="og:image" content={Title} />
