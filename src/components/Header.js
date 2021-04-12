06cba386e220232948a1fb1928b98976c5455ba0
JYOTI VAKARE <30766323+jyotivakare33@users.noreply.github.com>
2021-04-12 16:24:33 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,6 @@ import avatar from '../images/avatar.png';
 function Header() {
     return (
         <div>
-            <head>
-            <meta property="og:title" content="European Travel Destinations"/>
-            <meta property="og:description" content="Offering tour packages for individuals or groups."/>
-            <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg"/>
-            <meta property="og:url" content="http://euro-travel-example.com/index.htm"/>
-            </head>
             <header className="header">
                 <nav>
                     <p className="heading">The Developer Repository</p>
