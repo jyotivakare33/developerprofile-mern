67e062ee3d29aa5b020591f855708b4e29463296
JYOTI VAKARE <30766323+jyotivakare33@users.noreply.github.com>
2021-04-12 16:35:37 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,18 @@ import Search from '../components/Search';
 import DeveloperDetails from '../components/DeveloperDetails';
 import NotFound from '../components/NotFound';
 import Footer from '../components/Footer';
+import {Helmet} from "react-helmet";
+
 
 function Home() {
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Developer-Profile</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg"/>
+            </Helmet>
             <Header />
             <Search />
             <DeveloperDetails />
